e5f69720070db15d66c1b43d36760dbb5567ffee
leehansoll <pdcheck@naver.com>
2023-03-16 19:57:49 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer  from './components/Customer';
+
+const customer = {
+  'name': '나길동',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+}
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let`s develop management system!</h2>
-    </div>
+    <Customer
+      name={customer.name}
+      birthday={customer.birthday}
+      gender={customer.gender}
+      job={customer.job}    
+    />
   );
 }
 
