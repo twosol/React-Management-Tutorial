d46ab4f31e2579634a352b90537a3b5faf8300cf
leehansoll <pdcheck@naver.com>
2023-03-16 21:14:21 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import './App.css';
-import Customer  from './components/Customer';
+import Customer from './components/Customer';
 
-const customer = {
-  'name': '나길동',
-  'birthday': '961222',
-  'gender': '남자',
-  'job': '대학생'
-}
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '잠',
+    'birthday': '920101',
+    'gender': '남자',
+    'job': '대통령'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '온',
+    'birthday': '920202',
+    'gender': '남자',
+    'job': '총리'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '다',
+    'birthday': '920303',
+    'gender': '남자',
+    'job': '장관'
+  }
+]
 
 function App() {
   return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}    
-    />
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })
+      }
+    </div>
   );
 }
 
